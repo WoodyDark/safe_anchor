b09ea44d40be557d8790e659d0f7025e798285a6
woodydark <darkwoodpresents@gmail.com>
2020-06-18 23:05:26 +0800

    transferred logic to app/helpers folder

--- app/helpers/safe_anchor_helper.rb
+++ app/helpers/safe_anchor_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module UrlHelper
+module SafeAnchorHelper
   include ActionView::Helpers::UrlHelper
   alias rails_default_link_to link_to
 
--- lib/safe_anchor.rb
+++ lib/safe_anchor.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'safe_anchor/version'
-require 'safe_anchor/url_helper'
 
 module SafeAnchor
 end
