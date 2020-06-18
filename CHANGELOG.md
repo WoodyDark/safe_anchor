f920f819d6b8ec4e8217268f8320b52dc3eb6302
woodydark <darkwoodpresents@gmail.com>
2020-06-19 03:10:47 +0800

    fixed bug where passing blocks didn't work correctly

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Version 0.1.3
+
+- Fixed bug where passing blocks to link_to doesn't work
--- lib/safe_anchor/safe_anchor_url_helper.rb
+++ lib/safe_anchor/safe_anchor_url_helper.rb
@@ -4,8 +4,8 @@ module SafeAnchorUrlHelper
   include ActionView::Helpers::UrlHelper
   alias rails_default_link_to link_to
 
-  def link_to(*args, **kwargs)
-    anchor_tag = rails_default_link_to(*args, **kwargs)
+  def link_to(*args, **kwargs, &block)
+    anchor_tag = rails_default_link_to(*args, **kwargs, &block)
     return anchor_tag if kwargs[:keep_dirty]
 
     sanitize anchor_tag
